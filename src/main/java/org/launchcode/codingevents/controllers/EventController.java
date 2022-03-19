ce9b137fa97789ec47003ef9050d31a8fc6831fe
Abdullah-Rehman786 <Air711@live.com>
2022-03-19 18:09:12 -0500

    Complete 13 exercises

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("events")
@@ -17,11 +19,50 @@ public class EventController {
 
 
     @GetMapping
-    public String index(Model model) {
+    public String displayAllEvents(Model model) {
+        Map<String, String> eventsMap = new HashMap<>();
 
-        model.addAttribute("events", events);
+        eventsMap.put("StrangeLoop", "Strange Loop is a multi-disciplinary conference that brings" +
+                " together the developers and thinkers building tomorrow's" +
+                " technology in fields such as emerging languages, alternative" +
+                " databases, concurrency, distributed systems, security," +
+                " and the web.");
+        eventsMap.put("CodeDay St. Louis", "CodeDay is a worldwide event where student" +
+                " artists, programmers, musicians, actors, and writers get" +
+                " together to build apps and games.");
+        eventsMap.put("Washington DC Cyber Security Conference","Washington DC Cyber Security" +
+                " Conference brings high-level Cyber Security Training " +
+                "discovering cutting-edge security approaches, managing risk" +
+                " in the ever-changing threat of the cyber security workforce.");
+
+        model.addAttribute("eventsMap", eventsMap);
+
+        String strangeloop = "StrangeLoop";
+        String strangeloopAdr = "1001 Weird Circle Drive";
+        model.addAttribute("strangeloop", strangeloop);
+        model.addAttribute("strangeloopAdr", strangeloopAdr);
+
+
+        String codeDay = "CodeDay St. Louis";
+        String codeDayAdr = "1234 C0D3 Square Drive";
+        model.addAttribute("codeDay", codeDay);
+        model.addAttribute("codeDayAdr", codeDayAdr);
+
+
+        String cyber = "Washington DC Cyber Security Conference";
+        String cyberAdr = "0000 Secret Triangle Drive";
+        model.addAttribute("cyber", cyber);
+        model.addAttribute("cyberAdr", cyberAdr);
+
+
+//        Washington DC Cyber Security Conference
+
+
 
         return "events/index";
+//        model.addAttribute("events", events);
+//
+//        return "events/index";
     }
 
     // /events/create
