87adb0b3584263366ab77317ecf15471893f1bce
Abdullah-Rehman786 <Air711@live.com>
2022-03-22 19:14:04 -0500

    Complete Ch16 Enums

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -18,16 +18,19 @@ public class Event {
     @Email(message = "Invalid email. Try again")
     private String contactEmail;
 
+    private EventType type;
+
     public Event(){
         this.id = nextId;
         nextId++;
     }
 
-    public Event(String name, String description, String contactEmail) {
+    public Event(String name, String description, String contactEmail, EventType type) {
         this();
         this.name = name;
         this.description = description;
         this.contactEmail = contactEmail;
+        this.type = type;
     }
 
     public int getId() {
@@ -41,7 +44,6 @@ public class Event {
     public void setDescription(String description) {
         this.description = description;
     }
-
 
     public String getName() {
         return name;
@@ -57,6 +59,14 @@ public class Event {
 
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
+    }
+
+    public EventType getType() {
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
     }
 
     @Override
